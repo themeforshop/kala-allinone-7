498993d5a91407ef50ed4935d4515fecdaa74417
themeforshop <bao.nguyen@codespot.vn>
2022-04-05 17:05:03 +0700

    update code

--- assets/js/app.js
+++ assets/js/app.js
@@ -149,13 +149,6 @@ function sliderTheme(){
             focusOnSelect: true,
             responsive: [
                 {
-                  breakpoint: 1024,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                  }
-                },
-                {
                   breakpoint: 600,
                   settings: {
                     slidesToShow: 1,
